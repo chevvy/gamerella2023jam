f4d9b5cb0f824e594ffc0d132e9a3bf547467d99
BoulvardBisou <ma.dupont@live.ca>
2023-11-11 10:33:34 -0500

    ADD - Visual

---
+++ Rock/rock_visual.gd
@@ -0,0 +1,46 @@
+extends Node2D
+
+@export var vfx_hit : PackedScene
+@export var vfx_destroy : PackedScene
+
+@onready var sprite = $Base
+@onready var crack = $Base/Crack
+
+
+func _ready() -> void:
+#	hit_feeback()
+#	add_crack(3)
+#	print(Vector2.from_angle(deg_to_rad(90)))
+	pass # Replace with function body.
+
+func _process(delta: float) -> void:
+	pass
+
+func hit_feeback(_degree = null):
+	var vfx = vfx_hit.instantiate()
+	add_child(vfx)
+	vfx.emitting = true
+	if _degree == null :
+		tween_pos(0)
+	else: 
+		tween_pos(_degree)
+	
+	pass
+	
+
+func destroy_feedback():
+	vfx_destroy.emitting = true
+	pass
+
+func add_crack(_integrity = null):
+	if  _integrity == null :
+		crack.frame = 5
+	else :
+		crack.frame = _integrity
+
+func tween_pos(_degree = null):
+	var twe = create_tween()
+#	var dir = angle
+	print()
+	twe.tween_property(sprite,"position",Vector2.from_angle(deg_to_rad(_degree))*40,0.2) 
+	twe.tween_property(sprite,"position",Vector2(0,0),0.2) 
