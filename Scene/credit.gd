5dc4f6d8f3a0a6d0655db1e86fd8dc2d2d762d28
Vincent Chavanel Jobin <vincentcjobin@gmail.com>
2023-11-11 14:22:13 -0500

    refactor

--- Scene/credit.gd
+++ Scene/credit.gd
@@ -1,27 +1,9 @@
 extends Node2D
 
-@onready var menu_path = "res://Scene/menu.tscn"
-@onready var main_path = "res://main.tscn"
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-	
-
-
-
 
 func _on_button_pressed() -> void:
 	get_tree().quit()
-	pass # Replace with function body.
 
 
 func _on_button_2_pressed() -> void:
-	get_tree().change_scene_to_file(main_path)
-	pass # Replace with function body.
-	
+	LevelManager.load_main()
--- Scene/menu.gd
+++ Scene/menu.gd
@@ -1,31 +1,8 @@
-extends Node2D
-
-@onready var credit_path = "res://Scene/credit.tscn"
-@onready var main_path = "res://main.tscn"
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-	
-func load_main():
-	get_tree().change_scene_to_file(main_path)
-	pass
-
-func load_credit():
-	get_tree().change_scene_to_file(credit_path)
-	pass
-
+class_name Menu extends Node2D
 
 func _on_button_pressed() -> void:
-	load_main()
-	pass # Replace with function body.
+	LevelManager.load_main()
 
 
 func _on_button_2_pressed() -> void:
-	load_credit()
-	pass # Replace with function body.
+	LevelManager.load_credit()
