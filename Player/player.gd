2ae6548edd644651c61f4f90e7abf7dad4b47456
Vincent Chavanel Jobin <vincentcjobin@gmail.com>
2023-11-11 09:15:29 -0500

    navigation works

--- Player/player.gd
+++ Player/player.gd
@@ -1,27 +1,32 @@
 class_name Player extends Area2D
 
+@export var tile_map: TileMap
+
 @export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
+
+var tile_size = 48
+var inputs = {
+	"move_right": Vector2.RIGHT,
+	"move_left": Vector2.LEFT,
+	"move_up": Vector2.UP,
+	"move_down": Vector2.DOWN
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
-	
+	# position = position.snapped(Vector2.ONE * tile_size)
+	# position += Vector2.ONE * tile_size/2
 
+func _input(event):
+	if event.is_action_pressed("quit"):
+		get_tree().quit()
 
-func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
+func _unhandled_input(event):
+	for dir in inputs.keys():
+		if event.is_action_pressed(dir):
+			move(dir)
 
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		
-	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size)
+func move(dir):
+	position += inputs[dir] * tile_size
