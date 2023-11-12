03cee2ab0a0b89ca6fed88245c9c8129fbb9ded3
Vincent Chavanel Jobin <vincentcjobin@gmail.com>
2023-11-12 16:55:34 -0500

    pew pew

--- Terrain/Blocks/EnergyBlock/EnergyBlock.gd
+++ Terrain/Blocks/EnergyBlock/EnergyBlock.gd
@@ -2,8 +2,15 @@ class_name EnergyBlock extends Node2D
 
 @export var health_point_for_player = 5
 @export var rock_visual: RockVisual
+@export var base_rock: BaseBlock
+@export var destroy_timer: Timer
 
 func _on_base_block_hit_received(damageReceived, direction):
 	PlayerState.add_health(health_point_for_player)
 	rock_visual.destroy_feedback()
+	base_rock.health -= damageReceived
+	destroy_timer.start()	
+
+func _on_destroy_timer_timeout():
+	base_rock.queue_free()
 	queue_free()
