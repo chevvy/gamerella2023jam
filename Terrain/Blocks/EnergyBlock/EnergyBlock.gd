c94ceffe83e58d3a18fea7aa5ab5b47315dcdcd8
Vincent Chavanel Jobin <vincentcjobin@gmail.com>
2023-11-12 16:44:21 -0500

    energyblock wip

--- Terrain/Blocks/EnergyBlock/EnergyBlock.gd
+++ Terrain/Blocks/EnergyBlock/EnergyBlock.gd
@@ -1,8 +1,9 @@
 class_name EnergyBlock extends Node2D
 
 @export var health_point_for_player = 5
+@export var rock_visual: RockVisual
 
 func _on_base_block_hit_received(damageReceived, direction):
-	if damageReceived >= 1:
-		PlayerState.add_health(health_point_for_player)
-		queue_free()
+	PlayerState.add_health(health_point_for_player)
+	rock_visual.destroy_feedback()
+	queue_free()
