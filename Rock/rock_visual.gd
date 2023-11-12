ce3fbe7fdd354de1dbd9ca286effd15731417df8
BoulvardBisou <ma.dupont@live.ca>
2023-11-12 12:02:27 -0500

    ADD - SOUND
    cool

--- Rock/rock_visual.gd
+++ Rock/rock_visual.gd
@@ -3,31 +3,45 @@ class_name RockVisual extends Node2D
 @export var vfx_hit : PackedScene
 @export var vfx_destroy : PackedScene
 
+@export var sfx_hit : AudioStreamPlayer
+@export var sfx_explode : AudioStreamPlayer
+
+
 @onready var sprite = $Base
 @onready var crack = $Base/Crack
 
 var cracked_state = 5
 
 func _ready() -> void:
+	if sfx_hit == null :
+		print("SFX MISSING ON ROCK VISUAL")
+		return
+	if sfx_explode == null :
+		print("SFX MISSING ON ROCK VISUAL")
+		return
 	if vfx_hit == null :
 		print("VFX MISSING ON ROCK VISUAL")
+		return
 	if vfx_destroy == null :
 		print("VFX MISSING ON ROCK VISUAL")
+		return
+	
+	
 #	hit_feeback()
 #	add_crack(3)
 #	print(Vector2.from_angle(deg_to_rad(90)))
 	pass # Replace with function body.
 
 
-
-
 func destroy_feedback():
+	sfx_explode.play()
 	vfx_destroy.emitting = true
 	pass
 
 
 func hit_block_received(direction: float):
 	# ICI MARC
+	sfx_hit.play()
 	hit_feeback(direction)
 	hit_rock()
 	pass
