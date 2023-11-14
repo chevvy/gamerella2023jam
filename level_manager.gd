1eb531096a796aa7b1f7b4fe7646ed810616003b
Vincent Chavanel Jobin <vincentcjobin@gmail.com>
2023-11-13 20:51:21 -0500

    instantiate sfx

--- level_manager.gd
+++ level_manager.gd
@@ -3,12 +3,16 @@ extends Node
 @onready var menu_path = "res://Scene/menu.tscn"
 @onready var main_path = "res://main.tscn"
 @onready var credit_path = "res://Scene/credit.tscn"
+@onready var sfx = preload("res://Rock/rock_visual.tscn")
 
 func load_menu():
 	get_tree().change_scene_to_file(menu_path)
 	
 func load_main():
 	get_tree().change_scene_to_file(main_path)
+	# hopefully will help with shader cache
+	var ref = sfx.instantiate()
+	add_child(ref)
 	PlayerState.on_level_start()
 	
 func load_credit():
